d44a980b4d718934209c2dbb98a5eca0f3aa99b0
NLDPhat <1312422@student.hcmus.edu.vn>
2016-10-05 23:43:24 +0700

    Home_contact_about_bootstrap

--- MyStatus/config/routes.rb
+++ MyStatus/config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'about' => 'pages#about', as: :about
+  get 'contact' => 'pages#contact', as: :contact
+  get 'register' => 'users#index', as: :register
+  root 'pages#home'
 end
