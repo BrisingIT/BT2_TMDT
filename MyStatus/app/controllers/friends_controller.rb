2cf4fb1518c49ed642bdc3190b7dc1384753ea1f
BrisingIT <lonhthu74@gmail.com>
2016-10-11 13:53:03 +0700

    update

--- MyStatus/app/controllers/friends_controller.rb
+++ MyStatus/app/controllers/friends_controller.rb
@@ -12,7 +12,6 @@ class FriendsController < ApplicationController
 	end
 
 	def destroy
-		byebug
 		user = User.find(session[:current_user_id])
 		@friends = user.friendships.find(params[:id])
   		@friends.destroy
