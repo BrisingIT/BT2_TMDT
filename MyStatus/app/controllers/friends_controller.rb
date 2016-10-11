f596a46ebbd278bb894d9b50cce4c68fc6f5ca91
NLDPhat <1312422@student.hcmus.edu.vn>
2016-10-11 23:04:50 +0700

    Finish

--- MyStatus/app/controllers/friends_controller.rb
+++ MyStatus/app/controllers/friends_controller.rb
@@ -20,6 +20,11 @@ class FriendsController < ApplicationController
 		user = User.find(session[:current_user_id])
 		@friendship = user.friends.find_by_friend_id(params[:id])
   		@friendship.destroy
+
+  		user = User.find(params[:id])
+		@friendship = user.friends.find_by_friend_id(session[:current_user_id])
+  		@friendship.destroy
+  		
 		redirect_to :back
 	end
 
